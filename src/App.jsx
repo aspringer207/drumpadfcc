7b4c5289c5040c8d881ed4485d4f386d9c9e423f
aspringer207 <andrea.m.springer@gmail.com>
2024-02-26 16:05:14 -0500

    tests

--- src/App.jsx
+++ src/App.jsx
@@ -3,50 +3,56 @@ import React from 'react';
 import { Range } from './Range';
 import clipObj from './sounds';
 
-
+function clipList(){
+  const list = []
+  const myClips = clipObj;
+  for (const clip of clipObj) {
+    const el = `<media src="${clip.src}" id="${clip.key[0]}"></media>`
+    list.push(clip)
+  }
+  
+  return list
+}
 class App extends React.Component {
   constructor(props) {
     super()
     this.state = {
-      clips: [],
+      clips: ()=>clipList(),
       volume: 0
     }
+    this.handleClick = this.handleClick.bind(this)
   }
-  clipList() {
-    const list = []
-    const myClips = clipObj;
-    for (const clip of clipObj) {
-      const el = `<media src="${clip.src}" id="${clip.key[0]}"></media>`
-      list.push(clip)
-    }
-    
-    this.setState({clips: [...list]})
+  handleClick(event){
+    const btn = event.target.id;
+    const test = this.state.clips.filter((x)=> this.state.clips.key.includes(x))
+    console.log(test)
   }
+  
 
 
   render() {
     return (
-      <div fluid id="wrapper" className="App">
-        <div class="box">
-          <div class="mr">
+      <div id="wrapper" className="App">
+        <div className="box">
+          <div className="mr">
             <Range />
-            <SourceList />
+            <div id="clips" />
           </div>
         </div>
         <div className="box btn-box">
-          <div className="mr" fluid>
-            <button>Q</button>
+          <div className="mr">
+            <button onClick={('click', ()=> this.handleClick)}>Q</button>
             <button variant="danger">W</button>
             <button variant="success">E</button>
           </div>
 
-          <div className="mr" fluid>
+          <div className="mr">
             <button variant="info">A</button>
             <button>S</button>
             <button variant="warning">D</button>
           </div>
 
-          <div className="mr" fluid>
+          <div className="mr">
             <button variant="warning">Z</button>
             <button variant="danger">X</button>
             <button variant="info">C</button>
