0b515492afc217477e2ae91ff83264b1160b7690
aspringer207 <andrea.m.springer@gmail.com>
2024-02-26 17:22:51 -0500

    reconfigured ranger label

--- src/Range.jsx
+++ src/Range.jsx
@@ -4,8 +4,8 @@ import { Form, Col } from 'react-bootstrap';
 export function Range() {
   return (
     <>
-      <Form.Label for="volume">Range</Form.Label>
-      <Form.Range id="volume" min="0" max="100" step="5" />
+      <Form.Label>Range
+      <Form.Range id="volume" min="0" max="100" step="5" /></Form.Label>
     </>
   );
 }
