cb7dfed7ac7a9f45f2288a0eb69b96c33d0d4d83
aspringer207 <andrea.m.springer@gmail.com>
2024-02-26 17:19:20 -0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -19,51 +19,20 @@ class App extends React.Component {
     this.state = {
       current: null,
       clips: ()=>clipList(),
-      volume: 0
+      volume: 50
     }
-    this.handleClick = this.handleClick.bind(this)
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(event){
+
   }
   handleClick(event){
     event.preventDefault();
-    let curr;
-    switch(event.target.id){
-      case "sticks":
-        curr = document.getElementById("q");
-        this.setState({current: curr})
-        this.state.current.play();
-      case "crash":
-        curr = document.getElementById("w");
-        this.setState({current: curr})
-        curr.play();
-      case "crash2":
-        curr = document.getElementById("e");
-        this.setState({current: curr})
-        curr.play();
-      case "hihatopen":
-        curr = document.getElementById("a");
-        this.setState({current: curr})
-        curr.play();
-      case "hihatclosed":
-        curr = document.getElementById("s");
-        this.setState({current: curr})
-        curr.play();
-      case "ride":
-        curr = document.getElementById("d");
-        this.setState({current: curr})
-        curr.play();
-      case "snare":
-        curr = document.getElementById("z");
-        this.setState({current: curr})
-        curr.play();
-      case "kick":
-        curr = document.getElementById("x");
-        this.setState({current: curr})
-        curr.play();
-      case "tom":
-        curr = document.getElementById("c");
-        this.setState({current: curr})
-        curr.play();
-    }
+    let myInner = event.target.id.textContent;
+    console.log(myInner)
+    
+    
     
     
   }
@@ -75,7 +44,7 @@ class App extends React.Component {
       <div id="wrapper" className="App">
         <div className="box">
           <div className="mr">
-            <Range />
+            <Range onChange={this.handleChange} />
             <div id="clips" />
           </div>
         </div>
@@ -93,21 +62,21 @@ class App extends React.Component {
           </div>
 
           <div className="mr">
-            <button id="snare" type="button" variant="warning">Z</button>
-            <button id="kick" type="button" variant="danger">X</button>
-            <button id="tom" type="button" variant="info">C</button>
+            <button id="snare" type="button"  onClick={this.handleClick} variant="warning">Z</button>
+            <button id="kick" type="button"  onClick={this.handleClick} variant="danger">X</button>
+            <button id="tom" type="button"  onClick={this.handleClick} variant="info">C</button>
           </div>
         </div>
         <data>
-          <audio id="q" src="./src/sound/sticks.mp3"></audio>
-          <audio id="w" src="./src/sound/crash.mp3"></audio>
-          <audio id="e" src="./src/sound/crash2.mp3"></audio>
-          <audio id="a" src="./src/sound/hihatopen.mp3"></audio>
-          <audio id="s" src="./src/sound/hihatclosed.mp3"></audio>
-          <audio id="d" src="./src/sound/ride.mp3"></audio>
-          <audio id="z" src="./src/sound/snare.mp3"></audio>
-          <audio id="x" src="./src/sound/kick.mp3"></audio>
-          <audio id="c" src="./src/sound/tom.mp3"></audio>
+          <audio id="q" volume={this.state.volume} src="./src/sound/sticks.mp3"></audio>
+          <audio id="w" volume={this.state.volume} src="./src/sound/crash.mp3"></audio>
+          <audio id="e" volume={this.state.volume} src="./src/sound/crash2.mp3"></audio>
+          <audio id="a" volume={this.state.volume} src="./src/sound/hihatopen.mp3"></audio>
+          <audio id="s" volume={this.state.volume} src="./src/sound/hihatclosed.mp3"></audio>
+          <audio id="d" volume={this.state.volume} src="./src/sound/ride.mp3"></audio>
+          <audio id="z" volume={this.state.volume} src="./src/sound/snare.mp3"></audio>
+          <audio id="x" volume={this.state.volume} src="./src/sound/kick.mp3"></audio>
+          <audio id="c" volume={this.state.volume} src="./src/sound/tom.mp3"></audio>
 
         </data>
 
